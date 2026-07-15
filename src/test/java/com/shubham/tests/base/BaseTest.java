be9e2ffdf05224f1281e115ad725a5319d93c3ec
Shubham Thapa <shubhamthapa@Shubhams-MacBook-Pro.local>
2026-07-15 18:26:12 +0530

    adding some more debuggin statements

--- src/test/java/com/shubham/tests/base/BaseTest.java
+++ src/test/java/com/shubham/tests/base/BaseTest.java
@@ -9,21 +9,25 @@ import com.shubham.framework.driver.DriverManager;
 import com.shubham.framework.factory.BrowserFactory;
 
 public class BaseTest {
-    
+
     protected WebDriver driver;
-    
+
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         // Initialize WebDriver here using BrowserFactory and ConfigReader
         String browser = ConfigReader.getBrowser();
         driver = BrowserFactory.createDriver(browser);
         DriverManager.setDriver(driver);
         driver.manage().window().maximize();
-        driver.get(ConfigReader.getBaseUrl()); 
+        System.out.println("browser (System): " + System.getProperty("browser"));
+        System.out.println("headless (System): " + System.getProperty("headless"));
+
+        System.out.println("browser (Config): " + ConfigReader.getBrowser());
+        driver.get(ConfigReader.getBaseUrl());
     }
 
     @AfterMethod
-    public void tearDown(){
+    public void tearDown() {
         try {
             if (driver != null) {
                 driver.quit();
@@ -32,5 +36,5 @@ public class BaseTest {
             DriverManager.unload();
         }
 
-}
+    }
 }
